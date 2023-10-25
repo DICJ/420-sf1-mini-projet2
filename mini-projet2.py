162d6e4656365b772531a4106f57854059dd15e2
Nicolas Cote <NicolasCote@cegepjonquiere.ca>
2023-10-24 23:36:49 -0400

    first commit

---
+++ mini-projet2.py
@@ -0,0 +1,39 @@
+print("VOICI VOS REVENUS")
+print("-----------------")
+print("Enseignant - Salaire net = 40000$")
+print("Programmeur - Salaire net = 20000$")
+#mini projet 2 (programmation SIM)
+#nom prenom
+
+#-----------------------------------------------
+#Défintion de vos fonctions
+
+
+#-----------------------------------------------
+#votre programme commence-ici
+
+#liste des villes avec leur prix du logement
+liste_villes = [
+    {"Ville": "Montréal", "Montant (CAD)": 1500},
+    {"Ville": "Québec", "Montant (CAD)": 1200},
+    {"Ville": "Laval", "Montant (CAD)": 1100},
+    {"Ville": "Gatineau", "Montant (CAD)": 1000},
+    {"Ville": "Sherbrooke", "Montant (CAD)": 900},
+    {"Ville": "Trois-Rivières", "Montant (CAD)": 800},
+    {"Ville": "Saguenay", "Montant (CAD)": 750},
+    {"Ville": "Levis", "Montant (CAD)": 1400},
+]
+
+liste_automobiles = [
+    {"Marque": "HONDA", "Modèle": "Civic", "Prix (CAD)": 25000},
+    {"Marque": "HONDA", "Modèle": "Accord", "Prix (CAD)": 32000},
+    {"Marque": "HONDA", "Modèle": "CR-V", "Prix (CAD)": 29000},
+    
+    {"Marque": "TOYOTA", "Modèle": "Corolla", "Prix (CAD)": 24000},
+    {"Marque": "TOYOTA", "Modèle": "Camry", "Prix (CAD)": 31000},
+    {"Marque": "TOYOTA", "Modèle": "RAV4", "Prix (CAD)": 28000},
+    
+    {"Marque": "VOLKSWAGEN", "Modèle": "Golf", "Prix (CAD)": 27000},
+    {"Marque": "VOLKSWAGEN", "Modèle": "Passat", "Prix (CAD)": 33000},
+    {"Marque": "VOLKSWAGEN", "Modèle": "Tiguan", "Prix (CAD)": 30000}
+]
